6341043f22e81ca9f089073e851cb6cbe7127db4
Roman <rdik9044@gmail.com>
2021-02-25 10:45:51 +0200

    ready 4

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,12 @@ use App\Http\Controllers\SDKUseController;
 //    return view('welcome');
 //});
 
-Route::get('/',[ZohoController::class, 'index'])->name('index');
-Route::get('/init',[ZohoController::class, 'authorization_request'])->name('init');
-Route::get('/callback',[ZohoController::class, 'callback'])->name('callback');
-Route::get('/token',[ZohoController::class, 'access_token_request'])->name('access_token_request');
-Route::get('/work',[ZohoController::class, 'work'])->name('work');
+//Route::get('/',[ZohoController::class, 'index'])->name('index');
+//Route::get('/init',[ZohoController::class, 'authorization_request'])->name('init');
+//Route::get('/callback',[ZohoController::class, 'callback'])->name('callback');
+//Route::get('/token',[ZohoController::class, 'access_token_request'])->name('access_token_request');
+//Route::get('/work',[ZohoController::class, 'work'])->name('work');
 
-Route::get('/sdk',[SDKUseController::class, 'index'])->name('sdk.index');
+Route::get('/',[SDKUseController::class, 'index'])->name('index');
 Route::get('/sdk/callback',[SDKUseController::class, 'callback'])->name('sdk.callback');
 
